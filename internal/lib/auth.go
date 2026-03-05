4758bfaad470f50ec45a41961dcbf1f921a565d9
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-05 11:06:28 +0700

    feat: implement auth middleware to check authorization header

---
+++ internal/lib/auth.go
@@ -0,0 +1,22 @@
+package lib
+
+import (
+	"koda-b6-backend1/internal/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		token := ctx.GetHeader("Authorization")
+
+		if token == "" {
+			ctx.JSON(http.StatusBadRequest, models.Response{
+				Message: "Unauthorized",
+			})
+			return
+		}
+		ctx.Next()
+	}
+}
