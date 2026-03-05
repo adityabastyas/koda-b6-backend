91dc6f39620ae92047c84a95e6343b72f20a4460
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-05 10:59:00 +0700

    feat: add CORS middleware to allow frontend requests

---
+++ internal/lib/cors.go
@@ -0,0 +1,22 @@
+package lib
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CorsMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		ctx.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.Data(http.StatusOK, "", []byte(""))
+			return
+		}
+		ctx.Next()
+	}
+}
