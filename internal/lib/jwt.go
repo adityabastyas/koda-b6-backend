a145a6477007b6c95c25622abb02b24620eae5f9
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-09 06:08:47 +0700

    feat: add helper function to generate JWT token

--- internal/lib/jwt.go
+++ internal/lib/jwt.go
@@ -1,3 +1,20 @@
 package lib
 
-git
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = []byte("secret123")
+
+func GenerateToken(email string) (string, error) {
+	claims := jwt.MapClaims{
+		"email": email,
+		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(secretKey)
+}
