64b4c9cf370cc85aac45e81c1b86a9845b24876b
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-09 06:19:54 +0700

    feat: send JWT token in login response

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"koda-b6-backend1/internal/lib"
 	"koda-b6-backend1/internal/models"
 	"koda-b6-backend1/internal/service"
 	"net/http"
@@ -39,17 +40,22 @@ func (h *UserHandler) Login(ctx *gin.Context) {
 	var user models.User
 
 	if err := ctx.ShouldBindJSON(&user); err != nil {
-		ctx.JSON(http.StatusBadRequest, models.Response{Success: false, Message: "invalid body", Result: nil})
+		ctx.JSON(http.StatusBadRequest, models.Response{Success: false, Message: "invalid body"})
 		return
 	}
 
 	results, err := h.service.Login(user)
 	if err != nil {
-		ctx.JSON(http.StatusUnauthorized, models.Response{Success: false, Message: err.Error(), Result: nil})
+		ctx.JSON(http.StatusUnauthorized, models.Response{Success: false, Message: err.Error()})
 		return
 	}
 
-	ctx.JSON(http.StatusOK, models.Response{Success: true, Message: "login success", Result: results})
+	token, _ := lib.GenerateToken(results.Email)
+
+	ctx.JSON(http.StatusOK, models.Response{Success: true, Message: "login success", Result: gin.H{
+		"user":  results,
+		"token": token,
+	}})
 }
 
 func (h *UserHandler) GetAll(ctx *gin.Context) {
