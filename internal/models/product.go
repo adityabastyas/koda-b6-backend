e311274716c73ca082c6f8670bd5786b0b522a4d
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-09 12:48:34 +0700

    feat: add Product and ProductInput models for product data handling

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,10 +1,18 @@
 package models
 
 type Product struct {
-	ID          int      `json:"id"`
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	Price       int      `json:"price"`
-	Category    string   `json:"category"`
-	Image       []string `json:"image"`
+	ProductID   int    `json:"product_id" db:"product_id"`
+	Name        string `json:"name" db:"name"`
+	Description string `json:"description" db:"description"`
+	Price       int    `json:"price" db:"price"`
+	KategoryID  int    `json:"kategory_id" db:"kategory_id"`
+	ImageURL    string `json:"image_url" db:"image_url"`
+}
+
+type ProductInput struct {
+	KategoryID  int    `json:"kategory_id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+	ImageURL    string `json:"image_url"`
 }
