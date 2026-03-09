5ff99a5d5b99b9927db69fe6bae6aa6ae6c3131a
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-09 14:15:16 +0700

    feat: add Create method in product repository

--- internal/repository/product.go
+++ internal/repository/product.go
@@ -52,3 +52,10 @@ func (r *ProductRepository) GetByID(id int) (*models.Product, error) {
 	return &p, nil
 
 }
+
+func (r *ProductRepository) Create(input models.ProductInput) error {
+	query := `INSERT INTO products (kategory_id, name, description, price, image_url) VALUES ($1, $2, $3, $4, $5)`
+
+	_, err := lib.DB.Exec(context.Background(), query, input.KategoryID, input.Name, input.Description, input.Price, input.ImageURL)
+	return err
+}
