eea7a6e641ecdfee8eaba8db54eebfdfb2686aca
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-09 11:58:06 +0700

    feat: add lib connectDB and validation godotenv

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"koda-b6-backend1/internal/di"
+	"koda-b6-backend1/internal/lib"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		panic("file .env tidak ketemu")
+	}
+
+	lib.ConnectDB()
+
 	r := gin.Default()
 
 	di.Container(r)
