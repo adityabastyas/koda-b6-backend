cace4d3a40c37b488a8f9134d629abccfa9b58c8
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-10 04:30:44 +0700

    feat: add product route group

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,14 +7,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(r *gin.Engine, userHandler *handlers.UserHandler) {
+func SetupRoutes(r *gin.Engine, userHandler *handlers.UserHandler, productHandler *handlers.ProductHandler) {
 	r.POST("/register", userHandler.Register)
 	r.POST("/login", userHandler.Login)
 
 	userGroup := r.Group("/users")
 	userGroup.Use(lib.AuthMiddleware())
-
 	userGroup.GET("", userHandler.GetAll)
-
 	userGroup.POST("/upload", userHandler.UploadPhoto)
+
+	//product
+	productGroup := r.Group("/products")
+
+	productGroup.GET("", productHandler.GetAll)
+	productGroup.GET("/:id", productHandler.GetByID)
+
+	productGroup.Use(lib.AuthMiddleware())
+	productGroup.POST("", productHandler.Create)
+	productGroup.PATCH("/:id", productHandler.Update)
+	productGroup.DELETE("/:id", productHandler.Delete)
 }
