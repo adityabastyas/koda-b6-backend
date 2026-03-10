7b71c1cd75090e9f9b54232c36fec205a81f7c75
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-10 14:45:41 +0700

    feat: add method getbyid

--- internal/service/kategory.go
+++ internal/service/kategory.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"koda-b6-backend1/internal/models"
 	"koda-b6-backend1/internal/repository"
 )
@@ -17,4 +18,12 @@ func NewKategoryService(repo *repository.KategoryRepository) *KategoryService {
 
 func (s *KategoryService) GetAll() ([]models.Kategory, error) {
 	return s.repo.GetAll()
+}
+
+func (s *KategoryService) GetByID(id int) (*models.Kategory, error) {
+	if id <= 0 {
+		return nil, errors.New("id tidak valid")
+	}
+
+	return s.repo.GetByID(id)
 }
