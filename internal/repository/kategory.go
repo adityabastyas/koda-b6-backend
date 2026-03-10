d1649a25ed979a57efa802eae377f3911739637f
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-10 13:39:44 +0700

    feat: add method getbyid in kategory repository

--- internal/repository/kategory.go
+++ internal/repository/kategory.go
@@ -2,34 +2,55 @@ package repository
 
 import (
 	"context"
-	"koda-b6-backend1/internal/lib"
 	"koda-b6-backend1/internal/models"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type KategoryRepository struct{}
+type KategoryRepository struct {
+	db *pgxpool.Pool
+}
 
-func NewKategoryRepository() *KategoryRepository {
-	return &KategoryRepository{}
+func NewKategoryRepository(db *pgxpool.Pool) *KategoryRepository {
+	return &KategoryRepository{
+		db: db,
+	}
 }
 
 func (r *KategoryRepository) GetAll() ([]models.Kategory, error) {
 
 	query := `SELECT kategory_id, name FROM kategory`
 
-	rows, err := lib.DB.Query(context.Background(), query)
+	rows, err := r.db.Query(context.Background(), query)
 	if err != nil {
 		return nil, err
 	}
 
 	defer rows.Close()
 
-	kategory, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Kategory])
+	kategorys, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Kategory])
 	if err != nil {
 		return nil, err
 	}
 
-	return kategory, nil
+	return kategorys, nil
 
+}
+
+func (r *KategoryRepository) GetByID(id int) (*models.Kategory, error) {
+	query := `SELECT kategory_id, name FROM kategory WHERE kategory_id = $1`
+
+	rows, err := r.db.Query(context.Background(), query, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	kategory, err := pgx.CollectOneRow(rows, pgx.RowToStructByName[models.Kategory])
+	if err != nil {
+		return nil, err
+	}
+
+	return &kategory, nil
 }
