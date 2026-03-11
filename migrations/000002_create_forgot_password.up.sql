8b85f3dbee3be69391b84f7ff63ee958db073f11
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-11 10:19:53 +0700

    feat: add colums code in forgot password table

--- migrations/000002_create_forgot_password.up.sql
+++ migrations/000002_create_forgot_password.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS "forgot_password"(
   "id" SERIAL PRIMARY KEY,
   "email" VARCHAR(255) UNIQUE NOT NULL,
+  "code" VARCHAR(255) NOT NULL,
   "created_at" TIMESTAMP DEFAULT now() NOT NULL,
   "update_at" TIMESTAMP,
   "delete_at" TIMESTAMP
