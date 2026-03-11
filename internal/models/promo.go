d7b0e3fb4fde8446e6fad9f25fc70fa3d86778a4
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-11 15:05:51 +0700

    feat: add struct promo and promoInput

---
+++ internal/models/promo.go
@@ -0,0 +1,16 @@
+package models
+
+type Promo struct {
+	PromoID       int    `json:"promo_id" db:"promo_id"`
+	Title         string `json:"title" db:"title"`
+	Description   string `json:"description" db:"description"`
+	PromoType     string `json:"promo_type db:"promo_type"`
+	DiscountValue int    `json:"discount_value" db:"discount_value"`
+}
+
+type PromoInput struct {
+	Title         string `json:"title"`
+	Description   string `json:"description"`
+	PromoType     string `json:"promo_type"`
+	DiscountValue int    `json:"discount_value"`
+}
