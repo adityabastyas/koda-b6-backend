5f41a27aec6b282e0f39de4f6768f9ecf403ee25
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-12 09:23:06 +0700

    fix: remove method register

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -19,31 +19,6 @@ func NewUserHandler(service *service.UserService) *UserHandler {
 	}
 }
 
-// Register handle POST /register
-// @Summary Register user baru
-// @Tags auth
-// @Accept json
-// @Produce json
-// @Param user body models.UserRegisterInput true "User Register Input"
-// @Success 200 {object} models.Response
-// @Router /register [post]
-func (h *UserHandler) Register(ctx *gin.Context) {
-	var input models.UserRegisterInput
-
-	if err := ctx.ShouldBindJSON(&input); err != nil {
-		ctx.JSON(http.StatusBadRequest, models.Response{Success: false, Message: "invalid body", Result: nil})
-		return
-	}
-
-	if err := h.service.Register(input); err != nil {
-		ctx.JSON(http.StatusBadRequest, models.Response{Success: false, Message: err.Error(), Result: nil})
-		return
-	}
-
-	ctx.JSON(http.StatusOK, models.Response{Success: true, Message: "register success", Result: nil})
-
-}
-
 func (h *UserHandler) Login(ctx *gin.Context) {
 	var input models.UserLoginInput
 
