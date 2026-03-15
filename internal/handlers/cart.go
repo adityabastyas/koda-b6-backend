ab1d60d0a36d7de68d748caf66bce71dd5ce0c9e
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 06:13:30 +0700

    feat: add method GetByUserID

--- internal/handlers/cart.go
+++ internal/handlers/cart.go
@@ -4,6 +4,7 @@ import (
 	"koda-b6-backend1/internal/models"
 	"koda-b6-backend1/internal/service"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -35,5 +36,31 @@ func (h *CartHandler) GetAll(ctx *gin.Context) {
 		Success: true,
 		Message: "success",
 		Result:  carts,
+	})
+}
+
+func (h *CartHandler) GetByUserID(ctx *gin.Context) {
+	userID, err := strconv.Atoi(ctx.Param("user_id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "user id harus berupa angka",
+		})
+		return
+	}
+
+	cart, err := h.service.GetByUserID(userID)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  cart,
 	})
 }
