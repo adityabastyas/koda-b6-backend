bca3b34d3276f369c4e23e841b5568e4684a5bdf
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-15 19:09:59 +0700

    feat: add func NewPromoService

--- internal/service/promo.go
+++ internal/service/promo.go
@@ -5,3 +5,7 @@ import "koda-b6-backend1/internal/repository"
 type PromoService struct {
 	repo *repository.PromoRepository
 }
+
+func NewPromoService(repo *repository.PromoRepository) *PromoService {
+	return &PromoService{repo: repo}
+}
