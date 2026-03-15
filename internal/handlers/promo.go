cd8f9935b649afe559a990c22fbbc4cd16820b68
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 00:43:32 +0700

    feat: add method GetByID with validation

--- internal/handlers/promo.go
+++ internal/handlers/promo.go
@@ -4,6 +4,7 @@ import (
 	"koda-b6-backend1/internal/models"
 	"koda-b6-backend1/internal/service"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -34,5 +35,30 @@ func (h *PromoHandler) GetAll(ctx *gin.Context) {
 		Success: true,
 		Message: "success",
 		Result:  promos,
+	})
+}
+
+func (h *PromoHandler) GetByID(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "id harus berupa angka",
+		})
+		return
+	}
+
+	promo, err := h.service.GetByID(id)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  promo,
 	})
 }
