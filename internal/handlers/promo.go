190332e82c11ae1f816a59404fbec278a8a70bb8
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-15 19:15:55 +0700

    feat: add func NewPromoHandler

--- internal/handlers/promo.go
+++ internal/handlers/promo.go
@@ -5,3 +5,7 @@ import "koda-b6-backend1/internal/service"
 type PromoHandler struct {
 	service *service.PromoService
 }
+
+func NewPromoHandler(service *service.PromoService) *PromoHandler {
+	return &PromoHandler{service: service}
+}
