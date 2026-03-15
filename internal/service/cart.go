cdba2f062e4973f05524973e9f43205cd3614a79
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 06:07:38 +0700

    feat: add method GetById with validation

--- internal/service/cart.go
+++ internal/service/cart.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"koda-b6-backend1/internal/models"
 	"koda-b6-backend1/internal/repository"
 )
@@ -15,4 +16,11 @@ func NewCartService(repo *repository.CartRepository) *CartService {
 
 func (s *CartService) GetAll() ([]models.Cart, error) {
 	return s.repo.GetAll()
+}
+
+func (s *CartService) GetByUserID(userID int) (*models.Cart, error) {
+	if userID <= 0 {
+		return nil, errors.New("user id tidak valid")
+	}
+	return s.repo.GetByUserID(userID)
 }
