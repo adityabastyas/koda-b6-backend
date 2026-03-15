b838a1f0ba8a0b75f0360a18b907e8add2f1d3cb
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 06:06:52 +0700

    feat: add method GetAll

--- internal/service/cart.go
+++ internal/service/cart.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"koda-b6-backend1/internal/models"
 	"koda-b6-backend1/internal/repository"
 )
 
@@ -10,4 +11,8 @@ type CartService struct {
 
 func NewCartService(repo *repository.CartRepository) *CartService {
 	return &CartService{repo: repo}
+}
+
+func (s *CartService) GetAll() ([]models.Cart, error) {
+	return s.repo.GetAll()
 }
