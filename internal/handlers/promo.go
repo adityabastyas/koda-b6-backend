d859bc4d4d0519785a4239533e6c452436394a2d
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 01:07:17 +0700

    feat: add method update to ptomohandler with validation

--- internal/handlers/promo.go
+++ internal/handlers/promo.go
@@ -101,3 +101,36 @@ func (h *PromoHandler) Create(ctx *gin.Context) {
 		Message: "promo berhasil ditambahkan",
 	})
 }
+
+func (h *PromoHandler) Update(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "id harus berupa angka",
+		})
+		return
+	}
+
+	var input models.PromoInput
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "invalid body",
+		})
+		return
+	}
+
+	if err := h.service.Update(id, input); err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "promo berhasil diupdate",
+	})
+}
