712420c9412150b6432e08c0e6b222f9a84dec51
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 01:09:29 +0700

    feat: add method Delete

--- internal/handlers/promo.go
+++ internal/handlers/promo.go
@@ -143,3 +143,27 @@ func (h *PromoHandler) Update(ctx *gin.Context) {
 		Message: "promo berhasil diupdate",
 	})
 }
+
+func (h *PromoHandler) Delete(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "id harus berupa angka",
+		})
+		return
+	}
+
+	if err := h.service.Delete(id); err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "promo berhasil dihapus",
+	})
+}
