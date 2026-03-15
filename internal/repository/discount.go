5e4db08068f30fcb043a5ee573bdee5921548f82
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 01:27:11 +0700

    feat: add method GetAll in discount repository

--- internal/repository/discount.go
+++ internal/repository/discount.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jackc/pgx/v5"
+import (
+	"context"
+	"koda-b6-backend1/internal/models"
+
+	"github.com/jackc/pgx/v5"
+)
 
 type DiscountRepository struct {
 	DB *pgx.Conn
@@ -10,4 +15,21 @@ func NewDiscountRepository(db *pgx.Conn) *DiscountRepository {
 	return &DiscountRepository{
 		DB: db,
 	}
+}
+
+func (r *DiscountRepository) GetAll() ([]models.Discount, error) {
+	query := `SELECT discount_id, product_id, flash_sale, description, discount_rate FROM discount`
+
+	rows, err := r.DB.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	discounts, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Discount])
+	if err != nil {
+		return nil, err
+	}
+
+	return discounts, nil
 }
