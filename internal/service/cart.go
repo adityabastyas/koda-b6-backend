0264bd5ce06bd125cda9487f4fbf345865855757
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 06:06:04 +0700

    feat: add struct CartService and func NewCartService

---
+++ internal/service/cart.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"koda-b6-backend1/internal/repository"
+)
+
+type CartService struct {
+	repo *repository.CartRepository
+}
+
+func NewCartService(repo *repository.CartRepository) *CartService {
+	return &CartService{repo: repo}
+}
