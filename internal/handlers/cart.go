931c45ad39eec75ff799c64c96a81035d42bd92d
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 06:11:29 +0700

    feat: add method GetAll with validation

--- internal/handlers/cart.go
+++ internal/handlers/cart.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "koda-b6-backend1/internal/service"
+import (
+	"koda-b6-backend1/internal/models"
+	"koda-b6-backend1/internal/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type CartHandler struct {
 	service *service.CartService
@@ -8,4 +14,20 @@ type CartHandler struct {
 
 func NewCartHandler(service *service.CartService) *CartHandler {
 	return &CartHandler{service: service}
+}
+
+func (h *CartHandler) GetAll(ctx *gin.Context) {
+	carts, err := h.service.GetAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  carts,
+	})
 }
