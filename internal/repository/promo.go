f9a3dac98abb17b0c322994df45e1c732294ac9e
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-15 17:43:51 +0700

    feat: implement GetAll promo query

--- internal/repository/promo.go
+++ internal/repository/promo.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"context"
+	"koda-b6-backend1/internal/models"
+
 	"github.com/jackc/pgx/v5"
 )
 
@@ -12,4 +15,21 @@ func NewPromoRepository(db *pgx.Conn) *PromoRepository {
 	return &PromoRepository{
 		DB: db,
 	}
+}
+
+func (r *PromoRepository) GetAll() ([]models.Promo, error) {
+	query := `SELECT promo_id, title, description, promo_type, discount_value, FROM promo`
+
+	rows, err := r.DB.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	promos, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Promo])
+	if err != nil {
+		return nil, err
+	}
+
+	return promos, nil
 }
