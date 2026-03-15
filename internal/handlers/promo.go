1968a725ad0837002567c0e4dec3df56e84b77d1
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 00:29:27 +0700

    feat: add GetAll promo handler

--- internal/handlers/promo.go
+++ internal/handlers/promo.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "koda-b6-backend1/internal/service"
+import (
+	"koda-b6-backend1/internal/models"
+	"koda-b6-backend1/internal/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type PromoHandler struct {
 	service *service.PromoService
@@ -8,4 +14,20 @@ type PromoHandler struct {
 
 func NewPromoHandler(service *service.PromoService) *PromoHandler {
 	return &PromoHandler{service: service}
+}
+
+func (h *PromoHandler) GetAll(ctx *gin.Context) {
+	promos, err := h.service.GetAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  promos,
+	})
 }
