9d5f2e2a57c1a90bfc9563fdd4ae5ade2e60ea86
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 09:17:07 +0700

    feat: add struct and func NewTransactionService

---
+++ internal/service/transaction.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"koda-b6-backend1/internal/repository"
+)
+
+type TransactionService struct {
+	repo *repository.TransactionRepository
+}
+
+func NewTransactionService(repo *repository.TransactionRepository) *TransactionService {
+	return &TransactionService{repo: repo}
+}
