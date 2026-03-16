f47c4a5ba73d78aa4004cec6444f9dc0e5c12079
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 09:08:48 +0700

    feat: add method GetAll with query and validation

--- internal/repository/transaction.go
+++ internal/repository/transaction.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jackc/pgx/v5"
+import (
+	"context"
+	"koda-b6-backend1/internal/models"
+
+	"github.com/jackc/pgx/v5"
+)
 
 type TransactionRepository struct {
 	DB *pgx.Conn
@@ -8,4 +13,21 @@ type TransactionRepository struct {
 
 func NewTransactionRepository(db *pgx.Conn) *TransactionRepository {
 	return &TransactionRepository{DB: db}
+}
+
+func (r *TransactionRepository) GetAll() ([]models.Transaction, error) {
+	query := `SELECT transaction_id, user_id, promo_id, fullname, email, address, delivery_type, subtotal, tax, total, tanggal FROM transaction`
+
+	rows, err := r.DB.Query(context.Background(), query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	transactions, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Transaction])
+	if err != nil {
+		return nil, err
+	}
+
+	return transactions, nil
 }
