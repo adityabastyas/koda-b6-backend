2fd0605c0941bf1907a679b4da3b932a34920a99
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-16 11:47:16 +0700

    feat: add struct and func NewTransactionHandler

---
+++ internal/handlers/transaction.go
@@ -0,0 +1,11 @@
+package handlers
+
+import "koda-b6-backend1/internal/service"
+
+type TransactionHandler struct {
+	service *service.TransactionService
+}
+
+func NewTransactionHandler(service *service.TransactionService) *TransactionHandler {
+	return &TransactionHandler{service: service}
+}
