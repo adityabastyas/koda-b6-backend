7e258c61790d9a740986067b42a43b5f4d135491
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-18 13:48:56 +0700

    feat: add func ValidateToken with validation

--- internal/lib/jwt.go
+++ internal/lib/jwt.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -17,4 +18,27 @@ func GenerateToken(email string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString(secretKey)
+}
+
+func ValidateToken(tokenString string) (jwt.MapClaims, error) {
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("signin method tidak valid")
+		}
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return nil, errors.New("token tidak valid")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("token tidak valid")
+	}
+
+	return claims, nil
+
 }
