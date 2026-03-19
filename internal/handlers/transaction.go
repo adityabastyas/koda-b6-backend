43f5683597b14d9cb33251248fa9741b0b1d64b0
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-19 11:06:23 +0700

    feat: add method GetAll with validation

--- internal/handlers/transaction.go
+++ internal/handlers/transaction.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "koda-b6-backend1/internal/service"
+import (
+	"koda-b6-backend1/internal/models"
+	"koda-b6-backend1/internal/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type TransactionHandler struct {
 	service *service.TransactionService
@@ -9,4 +15,21 @@ type TransactionHandler struct {
 func NewTransactionHandler(service *service.TransactionService) *TransactionHandler {
 	return &TransactionHandler{
 		service: service}
+}
+
+func (h *TransactionHandler) GetAll(ctx *gin.Context) {
+	trasactions, err := h.service.GetAll()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  trasactions,
+	})
 }
