221dd623d87d7a0d26fab243a8185dcb43b3900d
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-20 14:57:59 +0700

    feat: add method GetById with validation

--- internal/handlers/transaction.go
+++ internal/handlers/transaction.go
@@ -4,6 +4,7 @@ import (
 	"koda-b6-backend1/internal/models"
 	"koda-b6-backend1/internal/service"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -37,5 +38,32 @@ func (h *TransactionHandler) GetAll(ctx *gin.Context) {
 		Success: true,
 		Message: "success",
 		Result:  trasactions,
+	})
+}
+
+
+func (h *TransactionHandler) GetByID(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "id harus berupa angka",
+		})
+		return
+	}
+
+	transaction, err := h,service.GetByID(id){
+		if err != nil{
+			ctx.JSON(http.StatusNotFound, models.Response{
+				Success: false,
+				Message: err.Error(),
+			})
+		}
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		success: true,
+		Message: "success",
+		Result: transaction,
 	})
 }
