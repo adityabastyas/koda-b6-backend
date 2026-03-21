6918f9a1de39fa879f85c482deb1cf96c9e234df
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-21 17:23:13 +0700

    feat: add struct CartItem and struct CartItemInput

--- internal/models/cart.go
+++ internal/models/cart.go
@@ -4,3 +4,19 @@ type Cart struct {
 	CartID int `json:"cart_id" db:"cart_id"`
 	UserID int `json:"user_id" db:"user_id"`
 }
+
+type CartItem struct {
+	CartItemID    int `json:"cart_item_id" db:"cart_item_id"`
+	CartID        int `json:"cart_id" db:"cart_id"`
+	ProductID     int `json:"product_id" db:"product_id"`
+	VariantID     int `json:"variant_id" db:"variant_id"`
+	ProductSizeID int `json:"product_size_id" db:"product_size_id"`
+	Quantity      int `json:"quantity" db:"quantity"`
+}
+
+type CartItemInput struct {
+	ProductID     int `json:"product_id"`
+	VariantID     int `json:"variant_id"`
+	ProductSizeID int `json:"product_size_id"`
+	Quantity      int `json:"quantity"`
+}
