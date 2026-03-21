5e61b23b47acb5bcb050ecc20091c9ae0ea53f63
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-21 16:48:56 +0700

    fix: repair lib to DB pgx.Conn

--- internal/repository/product.go
+++ internal/repository/product.go
@@ -2,22 +2,25 @@ package repository
 
 import (
 	"context"
-	"koda-b6-backend1/internal/lib"
 	"koda-b6-backend1/internal/models"
 
 	"github.com/jackc/pgx/v5"
 )
 
-type ProductRepository struct{}
+type ProductRepository struct {
+	DB *pgx.Conn
+}
 
-func NewProductRepository() *ProductRepository {
-	return &ProductRepository{}
+func NewProductRepository(db *pgx.Conn) *ProductRepository {
+	return &ProductRepository{
+		DB: db,
+	}
 }
 
 func (r *ProductRepository) GetAll() ([]models.Product, error) {
 	query := `SELECT product_id, kategory_id, name, description, price, image_url FROM products`
 
-	rows, err := lib.DB.Query(context.Background(), query)
+	rows, err := r.DB.Query(context.Background(), query)
 	if err != nil {
 		return nil, err
 	}
@@ -34,7 +37,7 @@ func (r *ProductRepository) GetAll() ([]models.Product, error) {
 func (r *ProductRepository) GetByID(id int) (*models.Product, error) {
 	query := `SELECT product_id, kategory_id, name, description,price, image_url FROM products WHERE product_id = $1`
 
-	rows, err := lib.DB.Query(context.Background(), query, id)
+	rows, err := r.DB.Query(context.Background(), query, id)
 	if err != nil {
 		return nil, err
 	}
@@ -52,21 +55,21 @@ func (r *ProductRepository) GetByID(id int) (*models.Product, error) {
 func (r *ProductRepository) Create(input models.ProductInput) error {
 	query := `INSERT INTO products (kategory_id, name, description, price, image_url) VALUES ($1, $2, $3, $4, $5)`
 
-	_, err := lib.DB.Exec(context.Background(), query, input.KategoryID, input.Name, input.Description, input.Price, input.ImageURL)
+	_, err := r.DB.Exec(context.Background(), query, input.KategoryID, input.Name, input.Description, input.Price, input.ImageURL)
 	return err
 }
 
 func (r *ProductRepository) Update(id int, input models.ProductInput) error {
 	query := `UPDATE products SET kategory_id=$1, name=$2, description=$3, price=$4, image_url=$5 WHERE product_id=$6`
 
-	_, err := lib.DB.Exec(context.Background(), query, input.KategoryID, input.Name, input.Description, input.Price, input.ImageURL, id)
+	_, err := r.DB.Exec(context.Background(), query, input.KategoryID, input.Name, input.Description, input.Price, input.ImageURL, id)
 	return err
 }
 
 func (r *ProductRepository) Delete(id int) error {
 	query := `DELETE FROM products WHERE product_id = $1`
 
-	_, err := lib.DB.Exec(context.Background(), query, id)
+	_, err := r.DB.Exec(context.Background(), query, id)
 	return err
 
 }
