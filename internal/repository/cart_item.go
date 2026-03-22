4dc7c1d991d42b1e2ad1cff519e8972a3b1e5b2d
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-22 17:53:18 +0700

    feat: add method GetByCartID with query and validation

--- internal/repository/cart_item.go
+++ internal/repository/cart_item.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jackc/pgx/v5"
+import (
+	"context"
+	"koda-b6-backend1/internal/models"
+
+	"github.com/jackc/pgx/v5"
+)
 
 type CartItemRepository struct {
 	DB *pgx.Conn
@@ -10,4 +15,21 @@ func NewCartItemRepository(db *pgx.Conn) *CartItemRepository {
 	return &CartItemRepository{
 		DB: db,
 	}
+}
+
+func (r *CartItemRepository) GetByCartID(cartID int) ([]models.CartItem, error) {
+	query := `SELECT cart_item_id, cart_id, product_id, variant_id, product_size_id, quantity FROM cart_item WHERE cart_id = $1`
+
+	rows, err := r.DB.Query(context.Background(), query, cartID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	items, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.CartItem])
+	if err != nil {
+		return nil, err
+	}
+
+	return items, nil
 }
