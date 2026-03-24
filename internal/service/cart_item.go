ddcf06e69e81da3f3547b4b5eda7614f07c56d23
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-24 18:41:15 +0700

    feat: add method GetByUserID with validation

--- internal/service/cart_item.go
+++ internal/service/cart_item.go
@@ -1,6 +1,10 @@
 package service
 
-import "koda-b6-backend1/internal/repository"
+import (
+	"errors"
+	"koda-b6-backend1/internal/models"
+	"koda-b6-backend1/internal/repository"
+)
 
 type CartItemService struct {
 	cartItemRepo *repository.CartItemRepository
@@ -12,4 +16,17 @@ func NewCartItemService(cartItemRepo *repository.CartItemRepository, cartRepo *r
 		cartItemRepo: cartItemRepo,
 		cartRepo:     cartRepo,
 	}
+}
+
+func (s *CartItemService) GetByUserID(userID int) ([]models.CartItem, error) {
+	if userID <= 0 {
+		return nil, errors.New("user id tidak valid")
+	}
+
+	cart, err := s.cartRepo.GetByUserID(userID)
+	if err != nil {
+		return nil, errors.New("cart tidak ditemukan")
+	}
+
+	return s.cartItemRepo.GetByCartID(cart.CartID)
 }
