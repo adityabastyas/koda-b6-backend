800fdf3b96a001358b8d3093afb74c157d45e026
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-26 13:26:19 +0700

    feat: add method GetByTransactionID with query and validation

--- internal/repository/transaction_product.go
+++ internal/repository/transaction_product.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jackc/pgx/v5"
+import (
+	"context"
+	"koda-b6-backend1/internal/models"
+
+	"github.com/jackc/pgx/v5"
+)
 
 type TransactionProductRepository struct {
 	DB *pgx.Conn
@@ -10,4 +15,21 @@ func NewTransactionProductRepository(db *pgx.Conn) *TransactionProductRepository
 	return &TransactionProductRepository{
 		DB: db,
 	}
+}
+
+func (r *TransactionProductRepository) GetByTransactionID(transactionID int) ([]models.TransactionProduct, error) {
+	query := `SELECT transaction_product_id, transaction_id, product_id, variant_id, product_size_id, quantity, price_at_purchase FROM transaction_product WHERE transaction_id = $1`
+
+	rows, err := r.DB.Query(context.Background(), query, transactionID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	products, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.TransactionProduct])
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
 }
