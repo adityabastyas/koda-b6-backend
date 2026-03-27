66591027be0288ec94218c3e33807862848b2d6d
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-27 14:03:48 +0700

    feat: add method GetByTransactionID with validation

--- internal/handlers/transaction_product.go
+++ internal/handlers/transaction_product.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "koda-b6-backend1/internal/service"
+import (
+	"koda-b6-backend1/internal/models"
+	"koda-b6-backend1/internal/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type TransactionProductHandler struct {
 	service *service.TransactionProductService
@@ -10,4 +17,31 @@ func NewTransactionProductHandler(service *service.TransactionProductService) *T
 	return &TransactionProductHandler{
 		service: service,
 	}
+}
+
+func (h *TransactionProductHandler) GetByTransactionID(ctx *gin.Context) {
+	transactionID, err := strconv.Atoi(ctx.Param("transaction_id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "transaction id harus berupa angka",
+		})
+		return
+	}
+
+	products, err := h.service.GetByTransactionID(transactionID)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  products,
+	})
+
 }
