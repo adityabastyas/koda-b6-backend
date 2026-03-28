0752d98c6f3c60bc0d370a6c674f542454a6aa67
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-28 17:28:50 +0700

    feat: add method GetByProductID with validation

--- internal/handlers/product_variant.go
+++ internal/handlers/product_variant.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "koda-b6-backend1/internal/service"
+import (
+	"koda-b6-backend1/internal/models"
+	"koda-b6-backend1/internal/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type ProductVariantHandler struct {
 	service *service.ProductVariantService
@@ -10,4 +17,30 @@ func NewProductVariantHandler(service *service.ProductVariantService) *ProductVa
 	return &ProductVariantHandler{
 		service: service,
 	}
+}
+
+func (h *ProductVariantHandler) GetByProductID(ctx *gin.Context) {
+	productID, err := strconv.Atoi(ctx.Param("product_id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "product id harus berupa angka",
+		})
+		return
+	}
+
+	variants, err := h.service.GetByProductID(productID)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  variants,
+	})
 }
