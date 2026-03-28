4534ddcb8e53d913661fe725365f1c4f56172f4f
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-03-28 16:23:19 +0700

    feat: add method GetProductID with select query

--- internal/repository/product_variant.go
+++ internal/repository/product_variant.go
@@ -1,6 +1,12 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"koda-b6-backend1/internal/models"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type ProductVariantRepository struct {
 	DB *pgxpool.Pool
@@ -10,4 +16,21 @@ func NewProductVariantRepository(db *pgxpool.Pool) *ProductVariantRepository {
 	return &ProductVariantRepository{
 		DB: db,
 	}
+}
+
+func (r *ProductVariantRepository) GetProductID(productID int) ([]models.ProductVariant, error) {
+	query := `SELECT variant_id, product_id, temperature, add_price FROM product_variant WHERE product_id = $1`
+
+	rows, err := r.DB.Query(context.Background(), query, productID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	variants, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.ProductVariant])
+	if err != nil {
+		return nil, err
+	}
+
+	return variants, nil
 }
