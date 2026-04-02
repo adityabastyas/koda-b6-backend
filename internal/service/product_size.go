34c3f059e3fe40a66428c6f5d65a37b6049d90fe
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-02 09:00:37 +0700

    feat: add method GetByProductID with validation

--- internal/service/product_size.go
+++ internal/service/product_size.go
@@ -1,6 +1,10 @@
 package service
 
-import "koda-b6-backend1/internal/repository"
+import (
+	"errors"
+	"koda-b6-backend1/internal/models"
+	"koda-b6-backend1/internal/repository"
+)
 
 type ProductSizeService struct {
 	repo *repository.ProductSizeRepository
@@ -10,4 +14,11 @@ func NewProductSizeService(repo *repository.ProductSizeRepository) *ProductSizeS
 	return &ProductSizeService{
 		repo: repo,
 	}
+}
+
+func (s *ProductSizeService) GetByProductID(productID int) ([]models.ProductSize, error) {
+	if productID <= 0 {
+		return nil, errors.New("product id tidak valid")
+	}
+	return s.repo.GetByProductID(productID)
 }
