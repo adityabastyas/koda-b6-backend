8e2dd619c6ecb57706ff85e62f3eb380a9d5511b
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-02 08:26:11 +0700

    feat: add method GetByProductID with query sql

--- internal/repository/product_size.go
+++ internal/repository/product_size.go
@@ -1,6 +1,10 @@
 package repository
 
 import (
+	"context"
+	"koda-b6-backend1/internal/models"
+
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -12,4 +16,21 @@ func NewProductSizeRepository(db *pgxpool.Pool) *ProductSizeRepository {
 	return &ProductSizeRepository{
 		DB: db,
 	}
+}
+
+func (r *ProductSizeRepository) GetByProductID(productID int) ([]models.ProductSize, error) {
+	query := `SELECT product_size_id, product_id, name, add_price FROM product_size WHERE product_id = $1`
+
+	rows, err := r.DB.Query(context.Background(), query, productID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	sizes, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.ProductSize])
+	if err != nil {
+		return nil, err
+	}
+
+	return sizes, nil
 }
