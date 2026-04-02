aec6febd8935dcc233267f85bcc22490ba2c8091
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-02 09:09:25 +0700

    feat: add method GetByProductID with validation status

--- internal/handlers/product_size.go
+++ internal/handlers/product_size.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "koda-b6-backend1/internal/service"
+import (
+	"koda-b6-backend1/internal/models"
+	"koda-b6-backend1/internal/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type ProductSizeHandler struct {
 	service *service.ProductSizeService
@@ -10,4 +17,30 @@ func NewProductSizeHandler(service *service.ProductSizeService) *ProductSizeHand
 	return &ProductSizeHandler{
 		service: service,
 	}
+}
+
+func (h *ProductSizeHandler) GetByProductID(ctx *gin.Context) {
+	productID, err := strconv.Atoi(ctx.Param("product_id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "product id harus berupa angka",
+		})
+		return
+	}
+
+	sizes, err := h.service.GetByProductID(productID)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  sizes,
+	})
 }
