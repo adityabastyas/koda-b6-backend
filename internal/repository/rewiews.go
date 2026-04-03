9acb616cee65f559ba7dd8d22bb3613b60088159
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-03 11:14:29 +0700

    feat: add method GetByProductID with sql query and validation

--- internal/repository/rewiews.go
+++ internal/repository/rewiews.go
@@ -1,6 +1,12 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"koda-b6-backend1/internal/models"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type ReviewsRepository struct {
 	DB *pgxpool.Pool
@@ -10,4 +16,21 @@ func NewReviewsRepository(db *pgxpool.Pool) *ReviewsRepository {
 	return &ReviewsRepository{
 		DB: db,
 	}
+}
+
+func (r *ReviewsRepository) GetByProductID(productID int) ([]models.Reviews, error) {
+	query := `SELECT reviews_id, product_id, user_id, message, rating FROM reviews WHERE product_id = $1`
+
+	rows, err := r.DB.Query(context.Background(), query, productID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	reviews, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Reviews])
+	if err != nil {
+		return nil, err
+	}
+
+	return reviews, nil
 }
