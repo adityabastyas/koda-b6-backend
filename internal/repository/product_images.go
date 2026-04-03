002e14bd00d66e192c87a499d735b294bc8edf7d
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-03 09:52:34 +0700

    feat: add method GetByProductID with query sql

--- internal/repository/product_images.go
+++ internal/repository/product_images.go
@@ -1,6 +1,12 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"koda-b6-backend1/internal/models"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type ProductImagesRepository struct {
 	DB *pgxpool.Pool
@@ -10,4 +16,21 @@ func NewProductImagesRepository(db *pgxpool.Pool) *ProductImagesRepository {
 	return &ProductImagesRepository{
 		DB: db,
 	}
+}
+
+func (r *ProductImagesRepository) GetByProductID(productID int) ([]models.ProductImages, error) {
+	query := `SELECT product_images_id, product_id, path FROM product_images WHERE product_id = $1`
+
+	rows, err := r.DB.Query(context.Background(), query, productID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	images, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.ProductImages])
+	if err != nil {
+		return nil, err
+	}
+
+	return images, nil
 }
