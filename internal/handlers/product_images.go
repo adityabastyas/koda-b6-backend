4067d367cac7ece82772bda9596cbcdb5be59b94
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-03 10:15:06 +0700

    feat: add method GetByProductID with validation status

--- internal/handlers/product_images.go
+++ internal/handlers/product_images.go
@@ -1,6 +1,13 @@
 package handlers
 
-import "koda-b6-backend1/internal/service"
+import (
+	"koda-b6-backend1/internal/models"
+	"koda-b6-backend1/internal/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 type ProductImagesHandler struct {
 	service *service.ProductImagesService
@@ -10,4 +17,30 @@ func NewProductImagesHandler(service *service.ProductImagesService) *ProductImag
 	return &ProductImagesHandler{
 		service: service,
 	}
+}
+
+func (h *ProductImagesHandler) GetByProductID(ctx *gin.Context) {
+	productID, err := strconv.Atoi(ctx.Param("product_id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, models.Response{
+			Success: false,
+			Message: "product id harus berupa angka",
+		})
+		return
+	}
+
+	images, err := h.service.GetByProductID(productID)
+	if err != nil {
+		ctx.JSON(http.StatusNotFound, models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, models.Response{
+		Success: true,
+		Message: "success",
+		Result:  images,
+	})
 }
