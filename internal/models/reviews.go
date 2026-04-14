c81afbca72451a6668f4b6c06d45f352cdb69831
aditya bastyas mulya <adityabastyasmulya@gmail.com>
2026-04-14 23:19:10 +0700

    feat: add FullName and ProfilePic fields to Reviews model for enhanced user information

--- internal/models/reviews.go
+++ internal/models/reviews.go
@@ -1,11 +1,13 @@
 package models
 
 type Reviews struct {
-	ReviewsID int    `json:"reviews_id" db:"reviews_id"`
-	ProductID int    `json:"product_id" db:"product_id"`
-	UserID    int    `json:"user_id" db:"user_id"`
-	Message   string `json:"message" db:"message"`
-	Rating    int    `json:"rating" db:"rating"`
+	ReviewsID  int     `json:"reviews_id" db:"reviews_id"`
+	ProductID  int     `json:"product_id" db:"product_id"`
+	UserID     int     `json:"user_id" db:"user_id"`
+	Message    string  `json:"message" db:"message"`
+	Rating     int     `json:"rating" db:"rating"`
+	FullName   string  `json:"full_name" db:"full_name"`
+	ProfilePic *string `json:"profile_pic" db:"profile_pic"`
 }
 
 type ReviewsInput struct {
